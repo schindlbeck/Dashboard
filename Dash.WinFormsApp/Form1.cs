aaeab4d753dbcf6f01582bc2aa4d56c856ddeb03
Larissa Dorn <dol33045@othr.onmicrosoft.com>
2021-11-11 16:57:12 +0100

    deleted false forms app

--- Dash.WinFormsApp/Form1.cs
+++ Dash.WinFormsApp/Form1.cs
@@ -7,44 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Dash.
 
 namespace Dash.WinFormsApp
 {
     public partial class Form1 : Form
     {
-        private List<DateTime> saturdays;
-        private List<DateTime> holidays;
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            saturdays = new List<DateTime>();
-            holidays = new List<DateTime>();
-        }
-
-        private void BtnAddSaturday_Click(object sender, EventArgs e)
-        {
-            var day = monthCalendarSaturdays.SelectionStart;
-            saturdays.Add(day);
-        }
-
-        private void BtnAddHolidays_Click(object sender, EventArgs e)
-        {
-            var startDay = monthCalendarHolidays.SelectionStart;
-            var endDay = monthCalendarHolidays.SelectionEnd;
-
-            for (DateTime day = startDay; day <= endDay; day.AddDays(1))
-            {
-                holidays.Add(day);
-            }
-        }
-
-        private void BtnStart_Click(object sender, EventArgs e)
-        {
-            WorkSchedule workSchedule = new();
-        }
+       
     }
 }
