b8fc0b8ee8b6d8b832a3565bfd7f119cb3fc43a0
schindlbeck <91056345+schindlbeck@users.noreply.github.com>
2021-11-12 10:33:19 +0100

    project Dash.DemoApp added

---
+++ Dash.DemoApp/Forms/DataOverview.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Erp.Prototype.Forms
+{
+    public partial class DataOverview : Form
+    {
+        public IConfigurationRoot Configuration { get; }
+
+        public DataOverview(IConfigurationRoot configuration)
+        {
+            InitializeComponent();
+            Configuration = configuration;
+        }
+
+        private void DataOverview_Load(object sender, EventArgs e)
+        {
+        }
+        
+    }
+}
