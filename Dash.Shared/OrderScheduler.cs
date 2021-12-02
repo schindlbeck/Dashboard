81cae0437cf1411aa1b28be3deeb50a72aa35146
Larissa Dorn <lariC96@web.de>
2021-12-02 19:15:45 +0100

    added more tests

--- Dash.Shared/OrderScheduler.cs
+++ Dash.Shared/OrderScheduler.cs
@@ -9,7 +9,7 @@ namespace Dash.Shared
     public class OrderScheduler
     {
         public List<OrderContainer> Orders { get; private set; }
-        private readonly Stack<LastChangedItem> lastChanged = new();
+        internal readonly Stack<LastChangedItem> lastChanged = new();
         
         private readonly PriorityDbContext priorityDbContext;
 
@@ -24,7 +24,25 @@ namespace Dash.Shared
             Orders.Add(order); 
         }
 
-        private void AddLastChangedItem(string key, int cwLast, int cwNow)
+        public OrderContainer GetOrder(string key)
+        {
+            return Orders.First(o => o.ListElement.KeyToString().Equals(key));
+        }
+
+        public async Task ChangeCW(string key, int newWeek, int year, bool isUndo)
+        {
+            var oldWeek = Orders.First(o => o.ListElement.KeyToString().Equals(key)).CurrentCW;
+            Orders.First(o => o.ListElement.KeyToString().Equals(key)).CurrentCW = newWeek;
+            Orders.First(o => o.ListElement.KeyToString().Equals(key)).CurrentYear = year;
+
+            priorityDbContext.Orders.First(o => o.Key.Equals(key)).CurrentCW = newWeek;
+            await priorityDbContext.SaveChangesAsync();
+
+            if (!isUndo)
+                AddLastChangedItem(key, oldWeek, newWeek);
+        }
+
+        internal void AddLastChangedItem(string key, int cwLast, int cwNow)
         {
             lastChanged.Push(new LastChangedItem() { Key = key, CwLast = cwLast, CwNow = cwNow });
         }
@@ -44,24 +62,6 @@ namespace Dash.Shared
         public void LastChangedUndid()
         {
             lastChanged.Pop();
-        }
-
-        public OrderContainer GetOrder(string key)
-        {
-            return Orders.First(o => o.ListElement.KeyToString().Equals(key));
-        }
-
-        public async Task ChangeCW(string key, int newWeek, int year, bool isUndo)
-        {
-            var oldWeek = Orders.First(o => o.ListElement.KeyToString().Equals(key)).CurrentCW;
-            Orders.First(o => o.ListElement.KeyToString().Equals(key)).CurrentCW = newWeek;
-            Orders.First(o => o.ListElement.KeyToString().Equals(key)).CurrentYear = year;
-
-            priorityDbContext.Orders.First(o => o.Key.Equals(key)).CurrentCW = newWeek;
-            await priorityDbContext.SaveChangesAsync();
-
-            if (!isUndo)
-                AddLastChangedItem(key, oldWeek, newWeek);
         }
     }
 
--- DashSharedTest/OrderSchedulerTest.cs
+++ DashSharedTest/OrderSchedulerTest.cs
@@ -29,13 +29,138 @@ namespace DashSharedTest
             scheduler.AddOrder(container);
 
             //Assert
-            Assert.Equal(scheduler.Orders.First(), container);
+            Assert.Equal(container, scheduler.Orders.First());
+        }
+
+        [Fact]
+        public void GetOrder_Test()
+        {
+            //Arrange
+            var optionsBuilder = new DbContextOptionsBuilder<PriorityDbContext>();
+            optionsBuilder.UseInMemoryDatabase("Test6");
+            var dbContext = new PriorityDbContext(optionsBuilder.Options);
+
+            OrderScheduler scheduler = new(dbContext);
+            PrioListElement element = new() { CWPlanned = 48, DeliveryDate = new DateTime(2021, 12, 2), OrderNr = "A001", Project = "P1" };
+            OrderContainer container = new(element);
+            scheduler.AddOrder(container);
+
+            //Act
+            var result = scheduler.GetOrder(container.ListElement.KeyToString());
+
+            //Assert
+            Assert.Equal(container, result);
+
+        }
+
+        [Fact]
+        public async void ChangeCW_Test()
+        {
+            //Arrange
+            var optionsBuilder = new DbContextOptionsBuilder<PriorityDbContext>();
+            optionsBuilder.UseInMemoryDatabase("Test7");
+            var dbContext = new PriorityDbContext(optionsBuilder.Options);
+
+            OrderScheduler scheduler = new(dbContext);
+            PrioListElement element = new() { CWPlanned = 48, DeliveryDate = new DateTime(2021, 12, 2), OrderNr = "A001", Project = "P1" };
+            OrderContainer container = new(element);
+            scheduler.AddOrder(container);
+
+            //Act
+            await scheduler.ChangeCW(container.ListElement.KeyToString(), 47, 2021, true);
+
+            //Assert
+            Assert.Equal(47, scheduler.Orders.First().CurrentCW);
         }
 
         [Fact]
         public void AddLastChangedItem_Test()
         {
+            //Arrange
+            var optionsBuilder = new DbContextOptionsBuilder<PriorityDbContext>();
+            optionsBuilder.UseInMemoryDatabase("Test2");
+            var dbContext = new PriorityDbContext(optionsBuilder.Options);
+
+            OrderScheduler scheduler = new(dbContext);
+            PrioListElement element = new() { CWPlanned = 48, DeliveryDate = new DateTime(2021, 12, 2), OrderNr = "A001", Project = "P1" };
+            OrderContainer container = new(element);
+            scheduler.AddOrder(container);
+
+
+            //Act
+            scheduler.AddLastChangedItem(container.ListElement.KeyToString(), 48, 47);
+
+            //Assert
+            Assert.Equal(47, scheduler.lastChanged.Peek().CwNow);
+            Assert.Equal(48, scheduler.lastChanged.Peek().CwLast);
+            Assert.Equal(container.ListElement.KeyToString(), scheduler.lastChanged.Peek().Key);
 
         }
+
+        [Fact]
+        public void GetLastChangedItem_Test()
+        {
+            //Arrange
+            var optionsBuilder = new DbContextOptionsBuilder<PriorityDbContext>();
+            optionsBuilder.UseInMemoryDatabase("Test3");
+            var dbContext = new PriorityDbContext(optionsBuilder.Options);
+
+            OrderScheduler scheduler = new(dbContext);
+            PrioListElement element = new() { CWPlanned = 48, DeliveryDate = new DateTime(2021, 12, 2), OrderNr = "A001", Project = "P1" };
+            OrderContainer container = new(element);
+            scheduler.AddOrder(container);
+            scheduler.AddLastChangedItem(container.ListElement.KeyToString(), 48, 47);
+
+            //Act
+            var result = scheduler.GetLastChangedItem();
+
+            //Assert
+            Assert.Equal(47, result.CwNow);
+            Assert.Equal(48, result.CwLast);
+            Assert.Equal(container.ListElement.KeyToString(), result.Key);
+        }
+
+        [Fact]
+        public void GetLastChangedItem_null_Test()
+        {
+            //Arrange
+            var optionsBuilder = new DbContextOptionsBuilder<PriorityDbContext>();
+            optionsBuilder.UseInMemoryDatabase("Test4");
+            var dbContext = new PriorityDbContext(optionsBuilder.Options);
+
+            OrderScheduler scheduler = new(dbContext);
+            PrioListElement element = new() { CWPlanned = 48, DeliveryDate = new DateTime(2021, 12, 2), OrderNr = "A001", Project = "P1" };
+            OrderContainer container = new(element);
+            scheduler.AddOrder(container);
+
+            //Act
+            var result = scheduler.GetLastChangedItem();
+
+            //Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void LastChangedUndid_Test()
+        {
+            //Arrange
+            var optionsBuilder = new DbContextOptionsBuilder<PriorityDbContext>();
+            optionsBuilder.UseInMemoryDatabase("Test5");
+            var dbContext = new PriorityDbContext(optionsBuilder.Options);
+
+            OrderScheduler scheduler = new(dbContext);
+            PrioListElement element = new() { CWPlanned = 48, DeliveryDate = new DateTime(2021, 12, 2), OrderNr = "A001", Project = "P1" };
+            OrderContainer container = new(element);
+            scheduler.AddOrder(container);
+            scheduler.AddLastChangedItem(container.ListElement.KeyToString(), 48, 47);
+
+            //Act
+            scheduler.LastChangedUndid();
+
+            //Assert
+            Assert.Empty(scheduler.lastChanged);
+        }
+
+        
     }
 }
