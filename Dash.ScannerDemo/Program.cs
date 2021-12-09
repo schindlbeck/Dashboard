50aa363f2a4a777fcf580cecf0863b1b599d9966
schindlbeck <91056345+schindlbeck@users.noreply.github.com>
2021-12-09 10:00:11 +0100

    scanner demo

---
+++ Dash.ScannerDemo/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace ScannerDemo
+{
+    class Program
+    {
+        static bool _continue = true;
+        static SerialPort _serialPort;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start *********************");
+
+            // Get a list of serial port names.
+            string[] ports = SerialPort.GetPortNames();
+
+            Console.WriteLine("The following serial ports were found:");
+
+            // Display each port name to the console.
+            foreach (string port in ports)
+            {
+                Console.WriteLine(port);
+            }
+
+            Console.ReadLine();
+
+            string name;
+            string message;
+            StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
+            Thread readThread = new Thread(Read);
+
+            // Create a new SerialPort object with default settings.
+            _serialPort = new SerialPort();
+
+            // set the appropriate properties.
+            _serialPort.PortName = ports[0];
+            _serialPort.BaudRate = 115200;
+            _serialPort.Parity = Parity.None;
+            _serialPort.DataBits = 8;
+            _serialPort.StopBits = StopBits.One;
+            _serialPort.Handshake = Handshake.RequestToSend;
+
+            // Set the read/write timeouts
+            _serialPort.ReadTimeout = 500;
+            _serialPort.WriteTimeout = 500;
+
+            _serialPort.Open();
+
+            readThread.Start();
+
+            Console.Write("Command: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Type QUIT to exit");
+
+            while (_continue)
+            {
+                message = Console.ReadLine();
+
+                if (stringComparer.Equals("quit", message))
+                {
+                    _continue = false;
+                }
+            }
+            _continue = false;
+            readThread.Join();
+            _serialPort.Close();
+        }
+
+        public static void Read()
+        {
+            while (_continue)
+            {
+                try
+                {
+                    string message = _serialPort.ReadLine();
+                    Console.WriteLine(message);
+                }
+                catch (TimeoutException) { }
+            }
+        }
+    }
+}
