1dde8143d19f9a07b6cd1cbffe90c4f768a5b798
Larissa Dorn <lariC96@web.de>
2021-12-10 11:15:11 +0100

    page for order overview

---
+++ BlazorServer/Pages/Overview.razor.cs
@@ -0,0 +1,18 @@
+﻿using BlazorServer.Models;
+using Dash.Data;
+using Dash.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorServer.Pages
+{
+    public partial class Overview
+    {
+        [Parameter] public List<OrderContainer> Orders { get; set; }
+
+        protected override void OnParametersSet()
+        {
+            Orders = service.DataModels;
+        }
+
+    }
+}
