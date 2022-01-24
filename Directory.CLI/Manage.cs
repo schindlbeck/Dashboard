427f98402817666969ecb7c7107de7e6ce0e0c08
Larissa Dorn <lariC96@web.de>
2022-01-24 11:57:39 +0100

    created cli for directory size

---
+++ Directory.CLI/Manage.cs
@@ -0,0 +1,30 @@
+﻿namespace Directory.CLI;
+
+using System.IO;
+
+internal class Manage
+{
+    private const string DIR = @"C:\Users\lado\source\repos\EvMobility";
+
+    public static void Execute()
+    {
+        var di = new DirectoryInfo(DIR);
+        var directories = di.GetDirectories();
+
+
+        foreach (var d in directories)
+        {
+            long totalsize = 0;
+
+            foreach (var file in d.GetFiles("*", SearchOption.AllDirectories))
+            {
+                totalsize += file.Length;
+            }
+
+            Console.WriteLine($"{d.Name} size: {totalsize} bytes");
+        }
+
+        Console.ReadLine();
+    }
+
+}
---
+++ Directory.CLI/Program.cs
@@ -0,0 +1,33 @@
+﻿using Directory.CLI;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        var rootCommand = new RootCommand();
+        
+
+        //Commands
+        var diskUsageCommand = new Command("du");
+        rootCommand.Add(diskUsageCommand);
+
+        //Command Options
+
+
+        //CommandHandler
+        diskUsageCommand.Handler = CommandHandler.Create(Manage.Execute);
+
+        //Return
+        try
+        {
+            return rootCommand.Invoke(args);
+        }
+        catch
+        {
+            return -1;
+        }
+    }
+
+}
